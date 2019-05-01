8937f7f10d5343d2176bad4e34b6b3d7d50ff098
Lagnesh Thakur <lagneshthakur@gmail.com>
2019-05-01 22:11:27 +0530

    cors handled and csv download in place

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -16,8 +16,26 @@ export class AppComponent {
   downloadData() {
     this.httpService.getData().subscribe((response) => {
       console.log(response);
+      this.createCSV(response, 'people.csv');
     }, (error) => {
       console.log(error);
     });
+  }
+
+  createCSV(data: any, filename: string) {
+    const replacer = (key, value) => value === null ? '' : value;
+    const header = Object.keys(data[0]);
+    const csv = [];
+    data.forEach(dataItem => {
+      csv.push(dataItem.firstName + ',' + dataItem.lastName);
+    });
+    csv.unshift(header.join(','));
+    const csvArray = csv.join('\r\n');
+    const blob = new Blob([csvArray], {type: 'text/csv' });
+    const blobURL = window.URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.download = 'people.csv';
+    anchor.href = blobURL;
+    anchor.click();
   }
 }
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var personsData = require('./personsData.js')
 
 const express = require('express')
+var cors = require('cors');
 const app = express()
 const port = 3000
+app.use(cors({
+    origin: 'http://localhost:4200'
+  }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
